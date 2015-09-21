649d48909f6c070df827e92ac07cda43a8714998
Matt Krueger <mpkrueger@gmail.com>
2015-09-20 17:58:04 -0700

    Created new project, first commit

---
+++ Bubs/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Bubs
+//
+//  Created by Matt Krueger on 9/20/15.
+//  Copyright © 2015 Matt Krueger. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
